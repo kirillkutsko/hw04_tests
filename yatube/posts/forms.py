2fbdd878597142a5204ad2bcfed904bb3d9c4f31
Кирилл Куцко <eads@mail.ru>
2022-05-13 14:34:25 +0300

    _

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -6,7 +6,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ("text", "group", "image")
+        fields = ("text", "group")
         labels = {
             "text": "Текст",
             "group": "Группа",
@@ -15,3 +15,4 @@ class PostForm(forms.ModelForm):
             "text": "Напишите текст поста",
             "group": "Выберите группу",
         }
+# , "image" с этим полем из 6го спринта не проходят тесты
