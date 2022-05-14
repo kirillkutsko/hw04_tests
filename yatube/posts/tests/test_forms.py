9d5fe48a104236e703eb3b7f3fceb19475e1b244
Кирилл Куцко <eads@mail.ru>
2022-05-14 03:15:17 +0300

    _

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,12 +1,19 @@
+import shutil
+import tempfile
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
 from ..models import Group, Post
 
 User = get_user_model()
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -17,16 +24,29 @@ class PostFormTests(TestCase):
             slug='test-slug',
             description='Тестовое описание',
         )
-
+        cls.small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        cls.uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=cls.small_gif,
+            content_type='image/gif'
+        )
         cls.post = Post.objects.create(
             author=cls.user,
-            text='Тестовый пост',
+            text='Текст постa',
             group=cls.group,
         )
 
     @classmethod
     def tearDownClass(cls):
         super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def setUp(self):
         self.guest_client = Client()
@@ -34,35 +54,46 @@ class PostFormTests(TestCase):
         self.authorized_client.force_login(self.user)
 
     def test_create_post(self):
-        """Валидная форма создает запись в Task."""
+        """Валидная форма создает запись в Post."""
         posts_count = Post.objects.count()
-
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         form_data = {
             'text': 'Текст поста',
             'group': self.group.id,
+            'image': uploaded,
         }
-
         response = self.authorized_client.post(
             reverse('posts:post_create'),
             data=form_data,
             follow=True
         )
-
         self.assertRedirects(response, reverse(
-            'posts:profile', args=[self.user.username])
+                'posts:profile', args=[self.user.username])
         )
-
         self.assertEqual(Post.objects.count(), posts_count + 1)
-
         self.assertTrue(
             Post.objects.filter(
                 text='Текст поста',
                 author=self.user,
-                group=self.group
+                group=self.group.id,
+                image='posts/small.gif'
             ).exists()
         )
 
     def test_edit_post(self):
+        """Валидная форма изменяет запись в Post."""
         post = PostFormTests.post
 
         form_data = {
--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,4 +1,9 @@
+import shutil
+import tempfile
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase
 from django.urls import reverse
 
@@ -9,6 +14,7 @@ from ..models import Group, Post
 User = get_user_model()
 FIRST_PAGE_POSTS = 13
 SECOND_PAGE_POSTS = 3
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
 class ViewsTests(TestCase):
@@ -21,10 +27,24 @@ class ViewsTests(TestCase):
             slug='test_slug',
             description='Тестовое описание'
         )
+        cls.small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        cls.uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=cls.small_gif,
+            content_type='image/gif',
+        )
         cls.post = Post.objects.create(
             author=cls.user,
             text='Тестовый пост',
             group=cls.group,
+            image=cls.uploaded,
         )
         cls.true_group = Group.objects.create(
             title='Тестовая группа2',
@@ -35,6 +55,7 @@ class ViewsTests(TestCase):
     @classmethod
     def tearDownClass(cls):
         super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def setUp(self):
         self.guest_client = Client()
